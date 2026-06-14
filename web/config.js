d1805db706e4006b9151151f410fb3a23b68698d
Bobby Plubell <bobbyplubell@gmail.com>
2026-06-13 19:22:06 -0500

    Wire the live relay into RELAYS (relay.proof-of-sheep.com)
    Connects the deployed swarm to the production relay so peers discover each
    other cross-machine and exchange sheep over direct WebRTC.

--- web/config.js
+++ web/config.js
@@ -4,4 +4,6 @@
 //   '/dns4/relay.example.com/tcp/443/wss/p2p/12D3KooW...'
 // The libp2p bundle (web/js/vendor/libp2p.js, ~620 KB) is only fetched when
 // this list is non-empty.
-export const RELAYS = [];
+export const RELAYS = [
+  '/dns4/relay.proof-of-sheep.com/tcp/443/wss/p2p/12D3KooWMfGMj9QJPdfQopxN18tUbBDpCmxJG6v3EmxMS4EPN4MU',
+];
